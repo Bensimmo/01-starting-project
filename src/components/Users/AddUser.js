824b469177e2267b559b56933c76c49c09785316
Ben Simpson <ben.simmo@hotmail.com>
2021-08-19 19:46:22 +1000

    first commit

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Card from "../UI/Card.js";
+import Button from "../UI/Button.js";
+
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredAge, enteredUsername);
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
